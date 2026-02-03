2029e2412f8cce1d89e642ce089df8c1e30e043c
Shivom Sharma <shars119@mcmaster.ca>
2026-02-03 18:50:03 -0500

    TCP Server examples

--- tutorial/echo_server/TCP/TCPClient.py
+++ tutorial/echo_server/TCP/TCPClient.py
@@ -1,15 +1,21 @@
 from socket import *
 
-host = ""
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((host, serverPort))
+
+def client():
+    host = ""
+    serverPort = 12000
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((host, serverPort))
+
+    while 1:
+        message = input("Input lowercase sentence:")
+        clientSocket.send(bytes(message, "utf-8"))
+
+        modifiedMessage = clientSocket.recv(2048)
+        print("From Server: " + modifiedMessage.decode("utf-8"))
+
+        if KeyboardInterrupt:
+            clientSocket.close()
 
 
-message = input("Input lowercase sentence:")
-clientSocket.send(bytes(message, "utf-8"))
-
-
-modifiedMessage = clientSocket.recv(2048)
-print("From Server: " + modifiedMessage.decode("utf-8"))
-clientSocket.close()
+client()
